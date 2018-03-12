c931ac28ea8237d33b3ec9895a7ae3ae100c4089
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-11 23:27:20 -0700

    Pass back the acronyms as structured data

--- lib/database.ex
+++ lib/database.ex
@@ -1,7 +1,53 @@
 defmodule Worker.Database do
-   @database_api Application.get_env(:worker, :database_api)
+  defmodule Credentials do
+    defstruct \
+    access_token: "",
+    bot_access_token: "",
+    bot_user_id: "",
+    bot_access_token: ""
+  end
 
-   def call(team_id) do
-     @database_api.get_acronyms(team_id)
-   end
+  defmodule Result do
+    defstruct \
+      name: "",
+      acronyms: [],
+      credentials: %Credentials{}
+  end
+
+  defmodule Acronym do
+    defstruct \
+    id: nil,
+    name: nil,
+    means: nil,
+    description: nil,
+    team_id: nil,
+    added_by: nil
+  end
+
+  @database_api Application.get_env(:worker, :database_api)
+
+  def call(team_id) do
+    acronyms = parse_acronyms(@database_api.get_acronyms(team_id))
+    result = %Result{
+      acronyms: acronyms
+    }
+    {:ok, result}
+  end
+
+  defp parse_acronyms({:ok, %Postgrex.Result{} = result}) do
+    parse_result(Acronym, result)
+  end
+
+  defp parse_acronyms(error) do
+    error
+  end
+
+  defp parse_result(type, %Postgrex.Result{columns: columns, rows: rows}) do
+    columns = Enum.map(columns, &String.to_atom/1)
+    Enum.map(rows, fn row ->
+      struct(type,
+        Enum.zip(columns, row) |> Enum.into(%{})
+      )
+    end)
+  end
 end
--- test/database_test.exs
+++ test/database_test.exs
@@ -1,6 +1,7 @@
 defmodule DatabaseTest do
   import Mox
   use ExUnit.Case
+  alias Worker.Database.{Acronym, Result}
   doctest Worker.Database
 
   setup :verify_on_exit!
@@ -20,13 +21,29 @@ defmodule DatabaseTest do
         }}
       end)
 
-      assert Worker.Database.call(@team_id) == {:ok, %Postgrex.Result{
-        columns: ["id", "name", "means", "description", "team_id", "added_by"],
-        rows: [
-          [2, "TLA", "three letter acronym", "the dumbest acronym", @team_id,
-          "Anil Kulkarni"],
-          [6, "HAM", "Jordan", "he's a ham", @team_id, "Anil Kulkarni"],
-        ]
-      }}
+      expected_acronyms =  [
+        %Worker.Database.Acronym{
+          added_by: "Anil Kulkarni",
+          description: "the dumbest acronym",
+          id: 2,
+          means: "three letter acronym",
+          name: "TLA",
+          team_id: "team1"
+        },
+        %Worker.Database.Acronym{
+          added_by: "Anil Kulkarni",
+          description: "he's a ham",
+          id: 6,
+          means: "Jordan",
+          name: "HAM",
+          team_id: "team1"
+        },
+      ]
+
+      expected = %Result{
+        acronyms: expected_acronyms
+      }
+
+      assert Worker.Database.call(@team_id) == {:ok, expected}
   end
 end
