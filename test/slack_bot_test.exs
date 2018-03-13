d37905bbc32ff1b51ef9880d49edf269602a5ba4
Anil Kulkarni <akulkarni@avvo.com>
2018-03-13 12:22:21 -0700

    Test sending a message to a group channel

--- test/slack_bot_test.exs
+++ test/slack_bot_test.exs
@@ -14,17 +14,17 @@ defmodule SlackBotTest do
 
   setup :verify_on_exit!
 
-  test "handle_event ignores messages sent from the bot" do
+  test "ignores messages sent from the bot" do
     m = message(%{user: %{ id: @bot_id }})
     assert {:ok, _} = Worker.SlackBot.handle_event(m, slack(), state())
   end
 
-  test "handle_event ignores messages sent from another bot" do
+  test "ignores messages sent from another bot" do
     m = message(%{subtype: "bot_message"})
     assert {:ok, %{}} = Worker.SlackBot.handle_event(m, slack(), state())
   end
 
-  test "handle_event adds a checkbox if one acronym is in the text" do
+  test "adds a checkbox if one acronym is in the text" do
     Worker.SlackWebApi.Reactions.MockClient
     |> expect(:add, fn @reaction, %{} ->
       {:ok}
@@ -32,36 +32,74 @@ defmodule SlackBotTest do
     assert {:ok, %{}} = Worker.SlackBot.handle_event(message(), slack(), state())
   end
 
-  test "handle_event does not add the reaction when there are no acronyms in the text" do
+  test "does not add the reaction when there are no acronyms in the text" do
     m = message(%{text: "There are no acronyms here"})
     assert {:ok, %{}} = Worker.SlackBot.handle_event(m, slack(), state())
   end
 
-  test "handle_event sends a DM when the question emoji is clicked in a channel" do
+  test "sends a DM when the question emoji is clicked in a channel" do
+    setup_channels_replies_mock()
+    setup_im_open_mock()
+    setup_chat_post_message_mock()
+    assert {:ok, %{}} = Worker.SlackBot.handle_event(reaction_message(), slack(), state())
+  end
+
+  test "sends a DM when the question emoji is clicked in a group message" do
+    setup_groups_replies_mock()
+    setup_im_open_mock()
+    setup_chat_post_message_mock()
+    m = reaction_message(
+      %{
+        item: %{
+          type: "message",
+          ts: @ts,
+          channel: @group,
+        }
+      }
+    )
+    assert {:ok, %{}} = Worker.SlackBot.handle_event(m, slack(), state())
+  end
+
+  defp setup_channels_replies_mock(text \\ "I need this by EOD.") do
     Worker.SlackWebApi.Channels.MockClient
     |> expect(:replies, fn (@channel, @ts, _) ->
       %{
         "messages" => [
           %{
-            "text" => "I need this by EOD."
+            "text" => text,
           },
         ],
       }
     end)
+  end
 
+  defp setup_groups_replies_mock(text \\ "I need this by EOD.") do
+    Worker.SlackWebApi.Groups.MockClient
+    |> expect(:replies, fn (@group, @ts, _) ->
+      %{
+        "messages" => [
+          %{
+            "text" => text,
+          },
+        ],
+      }
+    end)
+  end
+
+  defp setup_im_open_mock(user_id \\ @user_id) do
     Worker.SlackWebApi.Im.MockClient
-    |> expect(:open, fn @user_id, _ ->
+    |> expect(:open, fn ^user_id, _ ->
       %{
         "channel" => %{
           "id" => @dm_channel,
         },
       }
     end)
+  end
 
+  defp setup_chat_post_message_mock() do
     Worker.SlackWebApi.Chat.MockClient
     |> expect(:post_message, fn(@dm_channel, _, _) -> {:ok} end)
-
-    assert {:ok, %{}} = Worker.SlackBot.handle_event(reaction_message(), slack(), state())
   end
 
   defp reaction_message(%{} = options \\ %{}) do
