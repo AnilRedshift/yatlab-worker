89e21678b67ad5ee831d30155e82051ea940683a
Anil Kulkarni <akulkarni@avvo.com>
2018-04-03 15:25:59 -0700

    Change prod to use docker creds

--- config/prod.exs
+++ config/prod.exs
@@ -1,3 +1,8 @@
 use Mix.Config
 
-config :worker, prod: true
+config :worker,
+  prod: true,
+  db_host: "${DB_PORT_5432_TCP_ADDR}",
+  db_user: "postgres",
+  db_password: "${DB_ENV_POSTGRES_PASSWORD}",
+  db_path: "postgres"
